23dcae3af998ed09362137cad1c8dff7fbd5602a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-11-22 12:27:47 -0500

    Added Naive Single end stat algorithm
    This algorithm to determine the current state will not handle items that
    can exist in multiple states correctly. For example, if an item can be
    acted on by multiple parties A & B and be transitioned into multiple
    states, then the algorithm would fail to take into account the state and
    actions themselves.

--- src/main/scala/com.github.edgecaseberg/workflow/Workflow.scala
+++ src/main/scala/com.github.edgecaseberg/workflow/Workflow.scala
@@ -13,11 +13,30 @@ case class LogEntry(startState: State, endState: State, note: String, flowTaken:
 case class Workflow(states: List[State], actions: List[Action])
 
 object Workflow {
+	/** Determine the current state of a tracked object by it's Log and the workflow it adheres to
+	 *
+	 * Algorithm: 
+	 *
+	 * ∅ + A = S 
+	 * S + A = S →
+	 */
 	def determineCurrentState(log: Seq[LogEntry], workflow: Workflow) : Option[State] = {
+		val stack = new scala.collection.mutable.Stack[LogEntry]
 		if (log.isEmpty) {
 			None
 		} else {
-			None //TODO
+			for (entry <- log) {
+				entry.flowTaken match {
+					case Forward => stack.push(entry)
+					case Backward => stack.pop(); stack.push(entry)
+				}
+			}
+		}
+		println(stack)
+		if ( stack.isEmpty ) {
+			None
+		} else {
+			Some(stack.pop().endState)
 		}
 	}
 }
